fbe6062f17eea16c3e8331818ffc529de6841a55
Duan Li <inputx@gmail.com>
2022-12-15 17:10:02 +1100

    rewrite sprintf function with format, update go.mod to 1.18 version, add demo

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
 module github.com/duan-li/hue
 
-go 1.13
+go 1.18
--- sprintf.go
+++ sprintf.go
@@ -3,81 +3,97 @@ package hue
 import "fmt"
 
 // Sblackf returns a string with black color
-func Sblackf(s string) string {
+func Sblackf(format string, a ...any) string {
+	s := fmt.Sprintf(format, a...)
 	return fmt.Sprintf(Black, s)
 }
 
 // Sredf returns a string with red color
-func Sredf(s string) string {
+func Sredf(format string, a ...any) string {
+	s := fmt.Sprintf(format, a...)
 	return fmt.Sprintf(Red, s)
 }
 
 // Sgreenf returns a string with green color
-func Sgreenf(s string) string {
+func Sgreenf(format string, a ...any) string {
+	s := fmt.Sprintf(format, a...)
 	return fmt.Sprintf(Green, s)
 }
 
 // Syellowf returns a string with yellow color
-func Syellowf(s string) string {
+func Syellowf(format string, a ...any) string {
+	s := fmt.Sprintf(format, a...)
 	return fmt.Sprintf(Yellow, s)
 }
 
 // Sbluef returns a string with blue color
-func Sbluef(s string) string {
+func Sbluef(format string, a ...any) string {
+	s := fmt.Sprintf(format, a...)
 	return fmt.Sprintf(Blue, s)
 }
 
 // Smagentaf returns a string with magenta color
-func Smagentaf(s string) string {
+func Smagentaf(format string, a ...any) string {
+	s := fmt.Sprintf(format, a...)
 	return fmt.Sprintf(Magenta, s)
 }
 
 // Stealf returns a string with the color
-func Stealf(s string) string {
+func Stealf(format string, a ...any) string {
+	s := fmt.Sprintf(format, a...)
 	return fmt.Sprintf(Teal, s)
 }
 
 // Swhitef writes a string with black color
-func Swhitef(s string) string {
+func Swhitef(format string, a ...any) string {
+	s := fmt.Sprintf(format, a...)
 	return fmt.Sprintf(White, s)
 }
 
 // SblackBgf returns a string with black background color
-func SblackBgf(s string) string {
+func SblackBgf(format string, a ...any) string {
+	s := fmt.Sprintf(format, a...)
 	return fmt.Sprintf(BlackBg, s)
 }
 
 // SredBgf returns a string with red background color
-func SredBgf(s string) string {
+func SredBgf(format string, a ...any) string {
+	s := fmt.Sprintf(format, a...)
 	return fmt.Sprintf(RedBg, s)
 }
 
 // SgreenBgf returns a string with green background color
-func SgreenBgf(s string) string {
+func SgreenBgf(format string, a ...any) string {
+	s := fmt.Sprintf(format, a...)
 	return fmt.Sprintf(GreenBg, s)
 }
 
 // SyellowBgf returns a string with yellow background color
-func SyellowBgf(s string) string {
+func SyellowBgf(format string, a ...any) string {
+	s := fmt.Sprintf(format, a...)
 	return fmt.Sprintf(YellowBg, s)
 }
 
 // SblueBgf returns a string with blue background color
-func SblueBgf(s string) string {
+func SblueBgf(format string, a ...any) string {
+	s := fmt.Sprintf(format, a...)
 	return fmt.Sprintf(BlueBg, s)
 }
 
 // SmagentaBgf returns a string with magenta background color
-func SmagentaBgf(s string) string {
+func SmagentaBgf(format string, a ...any) string {
+	s := fmt.Sprintf(format, a...)
 	return fmt.Sprintf(MagentaBg, s)
 }
 
 // StealBgf returns a string with teal background color
-func StealBgf(s string) string {
+func StealBgf(format string, a ...any) string {
+	s := fmt.Sprintf(format, a...)
 	return fmt.Sprintf(TealBg, s)
 }
 
 // SwhiteBgf returns a string with white background color
-func SwhiteBgf(s string) string {
+func SwhiteBgf(format string, a ...any) string {
+	s := fmt.Sprintf(format, a...)
 	return fmt.Sprintf(WhiteBg, s)
 }
