84047c0ef12f41f35b27190f6657a2661003e175
Duan Li <inputx@gmail.com>
2022-12-27 21:16:57 +1100

    add test case, can test stdout

---
+++ println_test.go
@@ -0,0 +1,30 @@
+package hue
+
+import (
+	"io"
+	"os"
+	"testing"
+)
+
+func TestBlackln(t *testing.T) {
+	output := captureOutput(func() {
+		Blackln("test", "word")
+	})
+
+	if output != BlackStart+"test word"+BlackEnd+"\n" {
+		t.Error("Blackln failed")
+	}
+}
+
+func captureOutput(f func()) string {
+	rescueStdout := os.Stdout
+	r, w, _ := os.Pipe()
+	os.Stdout = w
+
+	f()
+
+	defer w.Close()
+	out, _ := io.ReadAll(r)
+	os.Stdout = rescueStdout
+	return string(out)
+}
